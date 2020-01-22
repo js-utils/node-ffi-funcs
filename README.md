b2d68eea53c6906b5ea422b96a79fba22319e7c2
五月的风 <578595193@qq.com>
2020-01-22 21:56:03 +0800

    SendMessage

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 * CWnd.FindWindow (className = null, windowName = null)
 * CWnd.FindWindowEx (hParent = null, hChild = null, className = null, windowName = null)
+* CWnd.GetClassName (hWnd)
 * CWnd.GetWindowText (hWnd)
+* CWnd.SendMessage(hWnd, Msg, wParam, lParam)
+* CWnd.C_FindWindow(hParent = null, hChild = null, shouldClassNameReg = null, shouldWindowNameReg = null)
--- test/CWnd.test.js
+++ test/CWnd.test.js
@@ -1,15 +1,22 @@
 const expect = require('chai').expect
 const CWnd = require('../lib/CWnd')
-const Helper = require('../lib/Helper')
+const { CString } = require('../lib/Helper')
 describe('CWnd TEST', function () {
   it('static FindWindow', async function() {
     let className = 'StandardFrame'
     let hWnd = CWnd.FindWindow(className, null)
     while (hWnd && !hWnd.isNull()) {
+      let className = CWnd.GetClassName(hWnd)
       let windowTitle = CWnd.GetWindowText(hWnd)
-      console.log('now window title: ', windowTitle)
+      console.log(`window className: ${className}, title: ${windowTitle}`)
       if (windowTitle.match('风驰万里1')) {
-        console.log('real find')
+        console.log('real find window')
+        let textWnd = CWnd.C_FindWindow(hWnd, null, 'RichEditComponent', null)
+        if (textWnd) {
+          console.log('real find RichEditComponent')
+          CWnd.SendMessage(textWnd, CWnd.Message.WM_SETTEXT, null, '好了')
+          CWnd.SendMessage(textWnd, CWnd.Message.WM_KEYDOWN, CWnd.Message.VK_RETURN, null)
+        }
         break
       } else {
         hWnd = CWnd.FindWindowEx(null, hWnd, className, null)
