59f3cca6a27893cad3e06f4461895a9edd81651d
sanmu <578595193@qq.com>
2020-02-24 00:00:15 +0800

    FEAT: GetRunningAppWithName & SetForegroundApp

--- test/CWnd.darwin.test.js
+++ test/CWnd.darwin.test.js
@@ -1,9 +1,16 @@
 const expect = require('chai').expect
 const { CWnd } = require('../lib')
 describe('CWnd TEST', function () {
-  // testGetInt testGetString
-  it('static GetDesktopWindow', async function() {
-    console.log(CWnd.TestGetInt())
-    console.log(CWnd.TestGetString())
+  // add
+  it('static add', async function() {
+    console.log('num:', CWnd.testInt(1))
+    console.log('string:', CWnd.testString("1"))
+    console.log('string:', CWnd.AllWindowInfo())
+  })
+  // GetRunningAppWithName
+  it('static GetRunningAppWithName', async function() {
+    let runningApp = CWnd.GetRunningAppWithName("风驰万里1", "Aliworkbench")
+    console.log(11, runningApp)
+    console.log(22, CWnd.SetForegroundApp(runningApp))
   })
 })
