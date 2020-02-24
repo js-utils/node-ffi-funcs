7c4813a7a911cb38f4309beaabcfe236345429f8
sanmu <578595193@qq.com>
2020-02-24 22:08:55 +0800

    GetOwnerPidWithWinName GetRunningAppWithOwnerPid

--- test/CWnd.darwin.test.js
+++ test/CWnd.darwin.test.js
@@ -6,19 +6,23 @@ describe('CWnd TEST', function () {
     console.log('num:', CWnd.testInt(1))
     console.log('string:', CWnd.testString("1"))
     console.log('string:', CWnd.AllWindowInfo())
+    console.log('.......')
   })
-  // GetRunningAppWithName
+  // GetRunningAppWithOwnerPid
   it('static GetRunningAppWithName', async function() {
-    let runningApp = CWnd.GetRunningAppWithName("风驰万里1", "Aliworkbench")
-    console.log(11, runningApp)
-    console.log(22, CWnd.SetForegroundApp(runningApp))
-    setTimeout(() => {
-      console.log('continue ...')
-      // 复制文字到粘贴板
-      console.log(CWnd.PasteboardCopyString("hello world"))
-      // 粘贴
-      console.log(CWnd.PostEventKey(CWnd.MACRO.KEY_CODE_V, CWnd.MACRO.FLAG_MASK_COMMAND))
-      console.log(CWnd.PostEventKey(CWnd.MACRO.KEY_CODE_RETURN))
-    }, 3000)
+    let ownerPid = CWnd.GetOwnerPidWithWinName("风驰万里1", "Aliworkbench")
+    if (ownerPid) {
+      let runningApp = CWnd.GetRunningAppWithOwnerPid(ownerPid)
+      console.log(11, runningApp)
+      console.log(22, CWnd.SetForegroundApp(runningApp))
+      setTimeout(() => {
+        console.log('continue ...')
+        // 复制文字到粘贴板
+        console.log(CWnd.PasteboardCopyString("hello world"))
+        // 粘贴
+        console.log(CWnd.PostEventKey(CWnd.MACRO.KEY_CODE_V, CWnd.MACRO.FLAG_MASK_COMMAND))
+        console.log(CWnd.PostEventKey(CWnd.MACRO.KEY_CODE_RETURN))
+      }, 3000)
+    }
   })
 })
