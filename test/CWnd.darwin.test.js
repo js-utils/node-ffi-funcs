a17b3ec360153c234bb2ed87462965e85932f706
sanmu <578595193@qq.com>
2020-02-24 23:38:58 +0800

    FEAT: GetWindowWithWinName

--- test/CWnd.darwin.test.js
+++ test/CWnd.darwin.test.js
@@ -7,22 +7,23 @@ describe('CWnd TEST', function () {
     console.log('string:', CWnd.testString("1"))
     console.log('string:', CWnd.AllWindowInfo())
     console.log('.......')
+    console.log(CWnd.GetWindowWithWinName("风驰万里1", "Aliworkbench"))
   })
   // GetRunningAppWithOwnerPid
-  it('static GetRunningAppWithName', async function() {
-    let ownerPid = CWnd.GetOwnerPidWithWinName("风驰万里1", "Aliworkbench")
-    if (ownerPid) {
-      let runningApp = CWnd.GetRunningAppWithOwnerPid(ownerPid)
-      console.log(11, runningApp)
-      console.log(22, CWnd.SetForegroundApp(runningApp))
-      setTimeout(() => {
-        console.log('continue ...')
-        // 复制文字到粘贴板
-        console.log(CWnd.PasteboardCopyString("hello world"))
-        // 粘贴
-        console.log(CWnd.PostEventKey(CWnd.MACRO.KEY_CODE_V, CWnd.MACRO.FLAG_MASK_COMMAND))
-        console.log(CWnd.PostEventKey(CWnd.MACRO.KEY_CODE_RETURN))
-      }, 3000)
-    }
-  })
+  // it('static GetRunningAppWithName', async function() {
+  //   let ownerPid = CWnd.GetOwnerPidWithWinName("风驰万里1", "Aliworkbench")
+  //   if (ownerPid) {
+  //     let runningApp = CWnd.GetRunningAppWithOwnerPid(ownerPid)
+  //     console.log(11, runningApp)
+  //     console.log(22, CWnd.SetForegroundApp(runningApp))
+  //     setTimeout(() => {
+  //       console.log('continue ...')
+  //       // 复制文字到粘贴板
+  //       console.log(CWnd.PasteboardCopyString("hello world"))
+  //       // 粘贴
+  //       console.log(CWnd.PostEventKey(CWnd.MACRO.KEY_CODE_V, CWnd.MACRO.FLAG_MASK_COMMAND))
+  //       console.log(CWnd.PostEventKey(CWnd.MACRO.KEY_CODE_RETURN))
+  //     }, 3000)
+  //   }
+  // })
 })
